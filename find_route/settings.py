00263331023af6216dcb962b79d1d9866a694eec
mtochenov <tochenovm@gmail.com>
2021-10-25 21:57:48 +0300

    fix some misprints in settings

--- find_route/settings.py
+++ find_route/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +25,7 @@ SECRET_KEY = 'django-insecure--(&8uczn+!3^dssdn0uvzj$^r$f5s84cl&vctlld*znz!tkvh!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,8 +36,6 @@ INSTALLED_APPS = [
     'trains',
     'accounts',
     'routes',
-    'rest_framework',
-    'rest_framework_simplejwt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
